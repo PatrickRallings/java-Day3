f7731825d11bdd5e70cb5e0214c40866f77e371b
Patrick Rallings <patrickrallings@Patricks-MBP.localdomain>
2022-05-04 10:17:19 -0400

    Added BoilingWater.class

---
+++ src/BoilingWater.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+public class BoilingWater {
+    public static void main(String[] args) {
+        System.out.println("Enter a number greater than 212:");
+        int num = BoilingWater.scanner();
+        BoilingWater.output(num);
+    }
+    public static int scanner() {
+        Scanner scanVal = new Scanner(System.in);
+        return scanVal.nextInt();
+    }
+    public static void output(int num) {
+        if (num > 212) {
+            System.out.println("The water is boiling!");
+        } else {
+            System.out.println("The water is not boiling.");
+        }
+    }
+}
